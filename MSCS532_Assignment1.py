d28b463b86c9a376c6425235560575f2676e2b7e
ShrisanKapali-Cumberlands <skapali32243@ucumberlands.edu>
2025-01-11 11:06:04 -0600

    complete assignment

--- MSCS532_Assignment1.py
+++ MSCS532_Assignment1.py
@@ -23,4 +23,33 @@ def monotonically_decreasing_insertion_sort(array):
 array1 = [5, 10, 20, 40, 1, 0, 60, 50, 200]
 array2 = [7, 14, 5, 2, 3, 45, 20, 16, 65, 75, 500, 100]
 array3 = [1000, 2000, 1500, 999, 6000, 4500, 1750, 6000]
-array3 = [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000]
+array4 = [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000]
+
+# Sort all the arrays and printing their values
+print("**************************************")
+print("Array 1 before sort ", array1)
+monotonically_decreasing_insertion_sort(array1)
+print("Array 1 after sort ", array1)
+print("**************************************")
+print("")
+
+print("**************************************")
+print("Array 2 before sort ", array2)
+monotonically_decreasing_insertion_sort(array2)
+print("Array 2 after sort ", array2)
+print("**************************************")
+print("")
+
+print("**************************************")
+print("Array 3 before sort ", array3)
+monotonically_decreasing_insertion_sort(array3)
+print("Array 3 after sort ", array3)
+print("**************************************")
+print("")
+
+print("**************************************")
+print("Array 4 before sort ", array4)
+monotonically_decreasing_insertion_sort(array4)
+print("Array 4 after sort ", array4)
+print("**************************************")
+print("")
